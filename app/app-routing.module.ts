b9962fad08e6017415f38f547d015992f090d357
Manichander Rama <manichander.rama@gmail.com>
2023-02-06 18:30:52 +0530

    Updated components

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { PackageHSComponent } from './package-hs/package-hs.component';
 import { PackageWComponent } from './package-w/package-w.component';
 
 const routes: Routes = [
-  {path:'',component:LoginComponent},
+  {path:'',component:HomepageComponent},
   {path:"login",component:LoginComponent},
   {path:"home",component:HomepageComponent},
   {path:"",component:HomepageComponent},
--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { UserserviceService } from '../userservice.service';
 declare var jQuery:  any;
@@ -15,7 +16,9 @@ export class LoginComponent implements OnInit {
   user1:any;
   confirmPassword:any;
    
-  constructor(private service:UserserviceService ){
+  private baseUrl = 'http://localhost:8080/api/auth/google';
+
+  constructor(private service:UserserviceService,private http: HttpClient){
      this.user= {userId:'',userName:'',emailId:'',mobileNumber:'',password:''};
      
     
@@ -61,4 +64,29 @@ export class LoginComponent implements OnInit {
     
   }
 
+  loginWithGoogle() {
+    const params = new URLSearchParams();
+    params.append('client_id', 'YOUR_CLIENT_ID');
+    params.append('redirect_uri', 'http://localhost:4200/callback');
+    params.append('scope', 'email profile');
+    params.append('response_type', 'code');
+    window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?${params.toString()}`;
+  }
+
+  getAccessToken(code: string) {
+    return this.http.post(`${this.baseUrl}/token`, { code })
+      .subscribe(
+        res => {
+          console.log(res);
+        },
+        err => {
+          console.log(err);
+        }
+      );
+  }
+
 }
+
+
+  
+  
