e2af289d3f3cb4d7cf2001bd5f31f83635479fbd
Manichander Rama <manichander.rama@gmail.com>
2023-02-09 17:30:32 +0530

    Snowsites component is updated.

--- app/snowsites/snowsites.component.ts
+++ app/snowsites/snowsites.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PackageService } from '../package.service';
-
+declare var jQuery:  any;
 @Component({
   selector: 'app-snowsites',
   templateUrl: './snowsites.component.html',
@@ -11,10 +11,18 @@ export class SnowsitesComponent implements OnInit {
 
   products: any;
   public carouselPFlag:boolean=true;
+  recentExp: any;
+  recentExp2: any;
+  recentExp1: any;
+  QTY: any;
   imgCollection: { image: string; thumbImage: string; alt: string; title: string; }[];
 
   constructor(private service:PackageService)    
   {
+    this.recentExp={};
+    this.recentExp1={};
+    this.recentExp2={};
+    this.QTY = 0;
     this.imgCollection = [
       {
         image: "assets/Images/snowsites/slider1.jpg",
@@ -42,6 +50,15 @@ export class SnowsitesComponent implements OnInit {
     this.carouselPFlag=ch;
   }
   explore(product: any){
-    alert("Explore Method is not yet added!");
+    this.recentExp=product;
+    jQuery('#cardModal').modal('show');
+  }
+  explore1(product: any){
+    this.recentExp1=product;
+    jQuery('#cardModal1').modal('show');
+  }
+  explore2(product: any){
+    this.recentExp2=product;
+    jQuery('#cardModal2').modal('show');
   }
 }
