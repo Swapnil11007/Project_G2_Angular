52da937fa762a85567172da2868314a130f6d2d8
SriramSaiGanesh <118200802+SriramSaiGanesh@users.noreply.github.com>
2023-02-09 16:38:27 +0530

    Dialoge Box and packageOptions Buttons added into Adventure, Beach, Desert, Piligrimage Components

--- app/adventure/adventure.component.ts
+++ app/adventure/adventure.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PackageService } from '../package.service';
-
+declare var jQuery:  any;
 @Component({
   selector: 'app-adventure',
   templateUrl: './adventure.component.html',
@@ -12,9 +12,16 @@ export class AdventureComponent implements OnInit {
   products: any;
   public carouselPFlag:boolean=true;
   imgCollection: { image: string; thumbImage: string; alt: string; title: string; }[];
+  recentExp: any;
+  recentExp1: any;
+  recentExp2: any;
+  QTY: number;
 
   constructor(private service:PackageService)    
   {
+    this.recentExp={};
+    this.recentExp1={};
+    this.QTY = 0;
     this.imgCollection = [
       {
         image: "assets/Images/adventure/slider1.jpg",
@@ -42,6 +49,11 @@ export class AdventureComponent implements OnInit {
     this.carouselPFlag=ch;
   }
   explore(product: any){
-    alert("Explore Method is not yet added!");
+    this.recentExp=product;
+    jQuery('#cardModal').modal('show');
+  }
+  explore1(product: any){
+    this.recentExp1=product;
+    jQuery('#cardModal1').modal('show');
   }
 }
--- app/desert/desert.component.ts
+++ app/desert/desert.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PackageService } from '../package.service';
+declare var jQuery:  any;
 
 @Component({
   selector: 'app-desert',
@@ -12,9 +13,17 @@ export class DesertComponent implements OnInit {
   products: any;
   public carouselPFlag:boolean=true;
   imgCollection: { image: string; thumbImage: string; alt: string; title: string; }[];
+  recentExp: any;
+  recentExp2: any;
+  recentExp1: any;
+  QTY: any;
 
   constructor(private service:PackageService)    
   {
+    this.recentExp={};
+    this.recentExp1={};
+    this.recentExp2={};
+    this.QTY = 0;
     this.imgCollection = [
       {
         image: "assets/Images/sliders/dubai.jpg",
@@ -37,6 +46,15 @@ export class DesertComponent implements OnInit {
     this.carouselPFlag=ch;
   }
   explore(product: any){
-    alert("Explore Method is not yet added!");
+    this.recentExp=product;
+    jQuery('#cardModal').modal('show');
+  }
+  explore1(product: any){
+    this.recentExp1=product;
+    jQuery('#cardModal1').modal('show');
+  }
+  explore2(product: any){
+    this.recentExp2=product;
+    jQuery('#cardModal2').modal('show');
   }
 }
