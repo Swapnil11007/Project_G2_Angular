6c844a36450eb5f9cb6175c375f23421c8dcc495
Ritesh <riteshsalunke11@gmail.com>
2023-02-09 17:24:48 +0530

    method for 3,5,7 implemented in Hs,honeymoon

--- app/beach/beach.component.ts
+++ app/beach/beach.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PackageService } from '../package.service';
+declare var jQuery:  any;
 
 @Component({
   selector: 'app-beach',
@@ -8,13 +9,20 @@ import { PackageService } from '../package.service';
 })
 export class BeachComponent implements OnInit {
   cards: any;
-
   products: any;
   public carouselPFlag:boolean=true;
   imgCollection: { image: string; thumbImage: string; alt: string; title: string; }[];
+  recentExp: any;
+  recentExp2: any;
+  recentExp1: any;
+  QTY: any;
+  total: any;
 
-  constructor(private service:PackageService)    
+  constructor(public service:PackageService)    
   {
+    this.recentExp={};
+    this.recentExp1={};
+    this.recentExp2={};
     this.imgCollection = [
       {
         image: "assets/Images/sliders/beach.jpeg",
@@ -27,8 +35,19 @@ export class BeachComponent implements OnInit {
         title: '',
         alt: 'Image 3'
       }
+
   ];
-  }
+
+  this.QTY = 0;
+
+}
+  // beachData: any[]=[]
+  // openDialog(card:any) {
+  //   console.log(card);
+  //   this.beachData.push(card);
+  //   this.dialog.open(DialogueComponent);
+  // }
+  
 
   ngOnInit(): void {
     this.service.getAllBeach().subscribe((data: any) => {this.products = data;});
@@ -37,6 +56,16 @@ export class BeachComponent implements OnInit {
     this.carouselPFlag=ch;
   }
   explore(product: any){
-    alert("Explore Method is not yet added!");
+    this.total = product.beachPrice * this.QTY;
+    this.recentExp=product;
+    jQuery('#cardModal').modal('show');
+  }
+  explore1(product: any){
+    this.recentExp1=product;
+    jQuery('#cardModal1').modal('show');
+  }
+  explore2(product: any){
+    this.recentExp2=product;
+    jQuery('#cardModal2').modal('show');
   }
 }
